b81f34d1b24ec5ea66809bd33c822e2488d67cbf
matthewjholmes <matthewjholmes@users.noreply.github.com>
2025-08-07 23:37:06 +0000

    deploy: 281976036ec3b21985ca06de86dc3679ac939540

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1754609656',resources: [
+    cacheName: 'chirpy-1754609825',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
